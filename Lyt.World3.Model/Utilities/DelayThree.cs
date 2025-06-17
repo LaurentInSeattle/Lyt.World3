5e98017ad3078da784e54704a4ea419ef6febc6a
Laurent <ly.testud@outlook.com>
2025-06-17 10:20:48 -0700

    Coded smoothing and delay methods ~ Untested yet

--- Lyt.World3.Model/Utilities/DelayThree.cs
+++ Lyt.World3.Model/Utilities/DelayThree.cs
@@ -1,70 +1,103 @@
 ﻿namespace Lyt.World3.Model.Utilities;
 
+// TODO: Refactor using Vectors and Matrices 
+
+/// <summary>
+///     Delay function of the 3rd order. 
+///     Returns a class that is callable as a function(see Call parameters) at a given step k.
+///     Computes the delayed vector out_arr from the input in_arr, at the step k.
+/// </summary>
 public class DelayThree
 {
+    private readonly double dt; // Time step 
+    private readonly double[] input; // input vector of the delay function.
+    private readonly List<double[]> output;
+    private readonly List<double[]> ANorm;
+    private readonly double[] BNorm;
+
+    public DelayThree(double[] input, double dt, double[] t)
+    {
+        this.input = input;
+        this.dt = dt;
+        this.output = [];
+        for (int i = 0; i < 3; i++)
+        {
+            double[] array = new double[t.Length];
+            Array.Fill<double>(array, 0);
+            output.Add(array);
+        }
+
+        this.ANorm = [];
+        double[] a1 = [-1.0, 0.0, 0.0];
+        this.ANorm.Add(a1);
+        double[] a2 = [1.0, -1.0, 0.0];
+        this.ANorm.Add(a2);
+        double[] a3 = [0.0, 1.0, -1.0];
+        this.ANorm.Add(a3);
+
+        this.BNorm = [1.0, 0.0, 0.0];
+    }
+
+    // k : current loop index.
+    // delay :  delay parameter. Higher delay increases smoothing.
+    public double Call(int k, double delay)
+    {
+        if (k == 0)
+        {
+            //  self.out_arr[0, :] = self.in_arr[0] * 3 / delay
+            foreach (double[] array in this.output)
+            {
+                array[0] = this.input[0] * 3.0 / delay;
+            }
+
+            return this.output[2][k];
+        }
+
+        // For now: Euler integration only  
+
+        // TEMP #1:     self.A_norm  @ self.out_arr[k - 1, :]
+        // Matrix multiply by vector should return a vector  
+        double[] aNorm = new double[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            double current = this.output[i][k - 1];
+            aNorm[i] =
+                this.ANorm[i][0] * current +
+                this.ANorm[i][1] * current +
+                this.ANorm[i][2] * current;
+        }
+
+        // TEMP #2:     self.B_norm * self.in_arr[k - 1]  : Vector multiplied by scalar 
+        double[] bNorm = new double[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            bNorm[i] = this.BNorm[i] * this.input[k - 1];
+        }
+
+        // Add vectors aNorm and bNorm into dout 
+        // dout = (self.A_norm  @ self.out_arr[k - 1, :] + self.B_norm * self.in_arr[k - 1])
+        double[] dout = new double[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            dout[i] = aNorm[i] + bNorm[i];
+        }
+
+
+        // Scalar multiply vector of dout 
+        // dout *= self.dt * 3 / delay
+        for (int i = 0; i < 3; ++i)
+        {
+            dout[i] *= this.dt * 3 / delay;
+        }
+
+        // self.out_arr[k, :] = self.out_arr[k - 1, :] + dout
+        int j = 0;
+        foreach (double[] array in this.output)
+        {
+            array[k] = array[k - 1] + dout[j++];
+        }
+
+        return this.output[2][k];
+    }
 }
-
-/*
-class Delay3:
-    """
-    Delay function of the 3rd order. Returns a class that is callable as a
-    function (see Call parameters) at a given step k.
-
-    Computes the delayed vector out_arr from the input in_arr, at the step k.
-
-    Parameters
-    ----------
-    in_arr : numpy ndarray
-        input vector of the delay function.
-    dt : float
-        time step.
-    t : numpy ndarray
-        time vector.
-    method : str, optional
-        "euler" or "odeint". The default is "euler".
-
-    Call parameters
-    ---------------
-    k : int
-        current loop index.
-    delay : float
-        delay parameter. Higher delay increases smoothing.
-
-    Call Returns
-    ------------
-    out_arr[k]
-
-    """
-
-    def __init__(self, in_arr, dt, t, method="euler"):
-        self.dt = dt
-        self.out_arr = np.zeros((t.size, 3))
-        self.in_arr = in_arr  # use in_arr as reference
-        self.method = method
-        if self.method == "euler":
-            self.A_norm = np.array([[-1., 0., 0.],
-                                    [1., -1., 0.],
-                                    [0., 1., -1.]])
-            self.B_norm = np.array([1, 0, 0])
-
-    def _init_out_arr(self, delay):
-        self.out_arr[0, :] = self.in_arr[0] * 3 / delay
-
-    def __call__(self, k, delay):
-        if k == 0:
-            self._init_out_arr(delay)
-        else:
-            if self.method == "odeint":
-                res = odeint(func_delay3, self.out_arr[k-1, :],
-                             [0, self.dt], args=(self.in_arr[k-1], delay))
-                self.out_arr[k, :] = res[1, :]
-            elif self.method == "euler":
-                dout = (self.A_norm  @ self.out_arr[k-1, :] +
-                        self.B_norm * self.in_arr[k-1])
-                dout *= self.dt*3/delay
-                self.out_arr[k, :] = self.out_arr[k-1, :] + dout
-
-        return self.out_arr[k, 2]
-
-*/
 
--- Lyt.World3.Model/Utilities/Smooth.cs
+++ Lyt.World3.Model/Utilities/Smooth.cs
@@ -1,78 +1,39 @@
 ﻿namespace Lyt.World3.Model.Utilities;
 
+/// <summary>
+///     Delay information function of the 1st order for smoothing. Also named DLINF1 in Dynamo.
+///     Returns a class that is callable as a function(see Call parameters) at a given step k.
+///     Computes the smoothed vector out_arr from the input array, at the step k.
+/// </summary>
 public sealed class Smooth
 {
+    private readonly double dt; // Time step 
+    private readonly double[] input; // input vector of the delay function.
+    private readonly double[] output;
+
+    public Smooth(double[] input, double dt, double[] t)
+    {
+        this.input = input;
+        this.dt = dt;
+        this.output = new double[t.Length];
+        Array.Fill<double>(this.output, 0);
+    }
+
+    // k : current loop index.
+    // delay :  delay parameter. Higher delay increases smoothing.
+    public double Call(int k, double delay)
+    {
+        if (k == 0)
+        {
+            double value = this.input[0];
+            this.output[0] = value;
+            return value;
+        }
+
+        // For now: Euler integration only  
+        double dout = this.input[k] - this.output[k - 1];
+        dout *= this.dt / delay;
+        this.output[k] = this.output[k - 1] + dout;
+        return this.output[k];
+    }
 }
-
-/*
-class Smooth:
-    """
-    Delay information function of the 1st order for smoothing. Also named
-    DLINF1 in Dynamo. Returns a class that is callable as a function (see
-    Call parameters) at a given step k.
-
-    Computes the smoothed vector out_arr from the input in_arr, at the step k.
-
-    Parameters
-    ----------
-    in_arr : numpy ndarray
-        input vector of the delay function.
-    dt : float
-        time step.
-    t : numpy ndarray
-        time vector.
-    method : str, optional
-        "euler" or "odeint". The default is "euler".
-
-    Call parameters
-    ---------------
-    k : int
-        current loop index.
-    delay : float
-        delay parameter. Higher delay increases smoothing.
-
-    Call Returns
-    ------------
-    out_arr[k]
-
-    """
-
-    def __init__(self, in_arr, dt, t, method="euler"):
-        self.dt = dt
-        self.out_arr = np.zeros((t.size,))
-        self.in_arr = in_arr  # use in_arr by reference
-        self.method = method
-
-    def __call__(self, k, delay):
-        if k == 0:
-            self.out_arr[k] = self.in_arr[k]
-        else:
-            if self.method == "odeint":
-                res = odeint(func_delay1, self.out_arr[k-1],
-                             [0, self.dt], args=(self.in_arr[k-1], delay))
-                self.out_arr[k] = res[1, :]
-            elif self.method == "euler":
-                dout = self.in_arr[k-1] - self.out_arr[k-1]
-                dout *= self.dt/delay
-                self.out_arr[k] = self.out_arr[k-1] + dout
-
-        return self.out_arr[k]
-
-
-DlInf1 = Smooth
-
-
-def func_delay3(out_, t_, in_, del_):
-    """
-    Computes the derivative of out_ at time t_, for the 3rd order delay. Used
-    in integration by odeint.
-
-    """
-    dout_ = np.zeros((3,))
-    dout_[0] = in_ - out_[0]
-    dout_[1] = out_[0] - out_[1]
-    dout_[2] = out_[1] - out_[2]
-
-    return dout_ * 3 / del_
-
-*/
