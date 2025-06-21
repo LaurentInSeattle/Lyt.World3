99bfe8aa3d083dba44022a55be53fd437a3b7994
Laurent <ly.testud@outlook.com>
2025-06-21 14:54:00 -0700

    Skeleton UI application - doing nothing for now

---
+++ Lyt.World3/Shell/ShellViewModel.cs
@@ -0,0 +1,103 @@
+﻿namespace Lyt.World3.Shell;
+
+//using static MessagingExtensions;
+//using static ViewActivationMessage;
+
+// https://stackoverflow.com/questions/385793/programmatically-start-application-on-login 
+
+public sealed partial class ShellViewModel : ViewModel<ShellView>
+{
+    private const int MinutesToMillisecs = 60 * 1_000;
+
+    //private readonly AstroPicModel astroPicModel;
+    private readonly IToaster toaster;
+
+    #region To please the XAML viewer 
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    // Should never be executed 
+    public ShellViewModel()
+    {
+    }
+#pragma warning restore CS8618 
+
+    #endregion To please the XAML viewer 
+
+    public ShellViewModel(/* AstroPicModel astroPicModel, */ IToaster toaster)
+    {
+        // this.astroPicModel = astroPicModel;
+        this.toaster = toaster;
+
+        //this.Messenger.Subscribe<ViewActivationMessage>(this.OnViewActivation);
+        //this.Messenger.Subscribe<ToolbarCommandMessage>(this.OnToolbarCommand);
+        this.Messenger.Subscribe<LanguageChangedMessage>(this.OnLanguageChanged);
+    }
+
+    private void OnLanguageChanged(LanguageChangedMessage message)
+    {
+        // We need to destroy and recreate the tray icon, so that it will be properly localized
+        // since its native menu will not respond to dynamic property changes 
+    }
+
+    // private void OnToolbarCommand(ToolbarCommandMessage _) => this.rotatorTimer.Reset();
+
+    public override void OnViewLoaded()
+    {
+        this.Logger.Debug("OnViewLoaded begins");
+
+        base.OnViewLoaded();
+        if (this.View is null)
+        {
+            throw new Exception("Failed to startup...");
+        }
+
+        // Select default language 
+        string preferredLanguage = "en-US"; //  this.astroPicModel.Language;
+        this.Logger.Debug("Language: " + preferredLanguage);
+        this.Localizer.SelectLanguage(preferredLanguage);
+        Thread.CurrentThread.CurrentCulture = new CultureInfo(preferredLanguage);
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo(preferredLanguage);
+
+        this.Logger.Debug("OnViewLoaded language loaded");
+
+        // Create all statics views and bind them 
+        ShellViewModel.SetupWorkflow();
+        this.Logger.Debug("OnViewLoaded SetupWorkflow complete");
+
+        // Ready 
+        //this.toaster.Host = this.View.ToasterHost;
+        //if (true)
+        //{
+        //    this.toaster.Show(
+        //        this.Localize("Shell.Ready"), this.Localize("Shell.Greetings"),
+        //        1_600, InformationLevel.Info);
+        //}
+
+
+        this.Logger.Debug("OnViewLoaded complete");
+    }
+
+    private static async void OnExit()
+    {
+        var application = App.GetRequiredService<IApplicationBase>();
+        await application.Shutdown();
+    }
+
+
+
+    private static void SetupWorkflow()
+    {
+    }
+
+#pragma warning disable IDE0079 
+#pragma warning disable CA1822 // Mark members as static
+
+    [RelayCommand]
+    public void OnClose() => OnExit();
+
+#pragma warning restore CA1822
+#pragma warning restore IDE0079
+
+    [ObservableProperty]
+    public bool mainToolbarIsVisible ; 
+}
---
+++ Lyt.World3/Shell/SplashWindow.axaml.cs
@@ -0,0 +1,6 @@
+namespace Lyt.World3.Shell;
+
+public partial class SplashWindow : Window
+{
+    public SplashWindow() => this.InitializeComponent();
+}
