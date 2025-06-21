1c3e6de2fb3b2c97f077a291063f46ff81691499
Laurent <ly.testud@outlook.com>
2025-06-21 16:36:25 -0700

    Added Scott Plot via nuget to UI app. Temp stuff in Shell view

--- Lyt.World3/Shell/ShellView.axaml.cs
+++ Lyt.World3/Shell/ShellView.axaml.cs
@@ -1,5 +1,9 @@
 ﻿namespace Lyt.World3.Shell;
 
+using ScottPlot;
+using ScottPlot.Avalonia;
+using ScottPlot.Plottables;
+
 public partial class ShellView : UserControl, IView
 {
     public ShellView()
@@ -11,6 +15,27 @@ public partial class ShellView : UserControl, IView
             {
                 viewModel.OnViewLoaded();
             }
+
+            double[] dataX = [1, 2, 3, 4, 5 , 6];
+            double[] dataY = [1, 4, 9, 16, 25, 36];
+            AvaPlot? avaPlot = this.Find<AvaPlot>("AvaPlot");
+            if (avaPlot is not null)
+            {
+                var plot = avaPlot.Plot;
+                plot.Add.Palette = new ScottPlot.Palettes.Penumbra(); 
+                Scatter scatter = plot.Add.Scatter(dataX, dataY);
+                scatter.LineWidth = 5;
+
+                // change figure colors
+                plot.FigureBackground.Color = Color.FromHex("#181818");
+                plot.DataBackground.Color = Color.FromHex("#1f1f1f");
+
+                // change axis and grid colors
+                plot.Axes.Color(Color.FromHex("#d7d7d7"));
+                plot.Grid.MajorLineColor = Color.FromHex("#404040");
+
+                avaPlot.Refresh();
+            } 
         };
     }
 }
