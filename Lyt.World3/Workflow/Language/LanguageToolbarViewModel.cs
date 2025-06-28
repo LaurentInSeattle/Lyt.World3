c36c5e7d97ad56205af34ab9f98f06129eebe3a5
Laurent <ly.testud@outlook.com>
2025-06-28 16:00:06 -0700

    Fix issue with static Relay Command

--- Lyt.World3/Workflow/Language/LanguageToolbarViewModel.cs
+++ Lyt.World3/Workflow/Language/LanguageToolbarViewModel.cs
@@ -4,6 +4,14 @@ using static MessagingExtensions;
 
 public sealed partial class LanguageToolbarViewModel : ViewModel<LanguageToolbarView>
 {
+#pragma warning disable IDE0079
+#pragma warning disable CA1822 // Mark members as static
+
+    // Relay commands MUST not be static 
+    // OR ELSE: Exception thrown: Common Language Runtime detected an invalid program.
     [RelayCommand]
-    public static void OnNext() => Select (ActivatedView.Results);
+    public void OnNext() => Select(ActivatedView.Results);
+
+#pragma warning restore CA1822
+#pragma warning restore IDE0079
 }
